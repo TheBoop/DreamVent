523bd3ab9506eead71d03a5115c82caf2779e1c9
Reyounited <cherna1993@gmail.com>
2016-04-21 02:18:03 -0700

    Added files via upload

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -65,20 +65,44 @@ class AuthController extends Controller
      */
     public function create(array $data)
     {
-        /*return USERS::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => bcrypt($data['password']),
-        ]);
-        */
-    
         $name = $data['name'];
         $email = $data['email'];
         $password = bcrypt($data['password']);
+        
+
+        $list[0] = "user_id";
+        $list[1] = "friendlist";
+        $list[2] = "followlist";
+        $list[3] = "contactlist";
+        $list[4] = "pagelist";
+        $list[5] = "holder5";
+        $list[6] = "holder6";
+        $list[7] = "holder7";
+        $list[8] = "holder8";
+        $list[9] = "holder9";
+            
+        for ($i=0; $i < 10; $i++)
+        {   
+            $affected = DB::insert('insert into USER_LIST
+                                (list_type) values 
+                                (?)',
+                                [$list[$i]]);
+            //store_user_id_value
+            $suv[$i] = intval(DB::getPdo()->lastInsertId());
+
+
+        }
+
+        $namer = "Bob Ross";
         $affected = DB::insert('insert into USER
-                                (username, email, password) values 
-                                (?, ?, ?)', 
-                                [$name, $email,$password]);
+                                (user_id, username, email, password, name, 
+                                friendlist_id, followlist_id, contactlist_id, 
+                                pagelist_id, holder_id5, holder_id6, holder_id7,
+                                holder_id8, holder_id9 ) values 
+                                (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', 
+                                [$suv[0], $name, $email, $password, $namer, $suv[1],
+                                 $suv[2], $suv[3], $suv[4], $suv[5], $suv[6], $suv[7], 
+                                 $suv[8], $suv[9]]);
 
     
     }
