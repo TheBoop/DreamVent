62fbf19316f64f851a0e1ad641bcdabb94ddfd42
Reyounited <cherna1993@gmail.com>
2016-04-21 14:05:50 -0700

    Update routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,37 @@
 <?php
 
-use Illuminate\Http\Request;
+/*
+|--------------------------------------------------------------------------
+| Routes File
+|--------------------------------------------------------------------------
+|
+| Here is where you will register all of the routes in an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
 
-Route::get('/', function () {
-    return view('welcome');
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+Route::group(['middleware' => ['web']], function () {
+
+    Route::get('/', function () {
+        return view('welcome');
+    })->middleware('guest');
+
+    Route::get('/tasks', 'TaskController@index');
+    Route::post('/task', 'TaskController@store');
+    Route::delete('/task/{task}', 'TaskController@destroy');
+
+    Route::auth();
+
 });
-
-
-
-//Route::post('/task', 'DreamventsController@store');
-//Route::delete('/task/{task}', 'DreamventsController@destroy');
-
-Route::auth();
-
-//Route::get('/home', 'HomeController@index');
