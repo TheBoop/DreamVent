51901c5f26541fce69c67f3df954fc9eed5837dd
Reyounited <cherna1993@gmail.com>
2016-04-21 21:26:04 -0700

    Update User.php

--- app/User.php
+++ app/User.php
@@ -1,26 +1,35 @@
 <?php
-
 namespace App;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
-
 class User extends Authenticatable
 {
+    protected $table = 'USER';
+    public $timestamps = false;
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'username', 'email', 'password', 'name', 'friendlist_id',
+        'followlist_id', 'contactlist_id', 'pagelist_id', 'holderid4', 
+        'holderid5', 'holderid6','holderid7','holderid8', 'holderid9',
     ];
-
     /**
-     * The attributes that should be hidden for arrays.
+     * The attributes excluded from the model's JSON form.
      *
      * @var array
      */
     protected $hidden = [
         'password', 'remember_token',
     ];
+    /**
+     * Get all of the tasks for the user.
+     */
+    public function account()
+    {
+        return $this->hasMany(Account::class);
+    }
 }
