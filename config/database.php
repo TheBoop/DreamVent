bd2161ba45dfe01fd629e0e0679fc5feffa5e632
Reyounited <cherna1993@gmail.com>
2016-04-23 03:48:29 -0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -56,7 +56,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'Dreamvents'),
+            'database' => env('DB_DATABASE', 'DreamVents'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', 'ucr'),
             'charset' => 'utf8',
