326cc54d98e190e59f083c5b3f9ca653074e97f6
Reyounited <cherna1993@gmail.com>
2016-04-25 22:05:25 -0700

    View Pictures Frontpage based on user

--- app/Http/Controllers/FrontPageController.php
+++ app/Http/Controllers/FrontPageController.php
@@ -7,6 +7,7 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 use App\AccountFrontPage;
+use App\Picture;
 use App\Repositories\AccountRepository;
 
 class FrontPageController extends Controller
