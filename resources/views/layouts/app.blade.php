17dd4d109f17d807dcfd95945bb580cb08964cf7
Reyounited <cherna1993@gmail.com>
2016-05-02 13:29:51 -0700

    merging

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,43 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Dreamvents</title>
-
-    <!-- Fonts -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
-    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
-
-    <!-- Styles -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
-    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
-
-    <style>
-        body {
-            font-family: 'Lato';
-            background-color: lightblue;
-        }
-        .fa-btn {
-            margin-right: 6px;
-        }
-    </style>
+    @include('includes.head')
+    @include('includes.styles')
 </head>
 <body id="app-layout">
     <nav class="navbar navbar-default">
         <div class="container">
-            <div class="navbar-header">
 
-                <!-- Collapsed Hamburger -->
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                    <span class="sr-only">Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
+            @include('includes.header')
 
+<<<<<<< HEAD
                 <!-- Branding Image -->
                 <a class="navbar-brand" href="{{ url('/') }}">
                     DreamVents
@@ -87,11 +60,23 @@
                     @endif
                 </ul>
             </div>
+=======
+>>>>>>> refs/remotes/origin/Matt_2
         </div>
     </nav>
 
-    @yield('content')
+    <div id="main" class="row">
+        <div id="sidebar" class="col-md-2">
+            @include('includes.sidebar')
+        </div>
 
+        <div id="content" class="col-md-10">
+            @yield('content')
+        </div>
+        
+    </div>
+    <!-- JavaScripts -->
+    @include('javascript.include_java')
 
 </body>
 </html>
