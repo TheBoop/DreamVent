bf276c9902638ced9a6c0865dcb246d039b99a47
FreeStyle3 <calvinucr@yahoo.com>
2016-05-02 09:15:37 +0000

    working

--- app/Http/Controllers/PostPageController.php
+++ app/Http/Controllers/PostPageController.php
@@ -9,7 +9,7 @@ use App\Picture; //Eloquent Model
 
 class PostPage extends Controller
 {
-    public function ViewImage($picture_id) {
+    public function ViewPost($picture_id) {
 		return view('post/post',  ['picture' => Picture::find($picture_id)]);
 	}
 	
