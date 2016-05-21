22375c719c0b53f7e7c7cd5065f725c1cca6a185
David Ding <davidxyding1@gmail.com>
2016-05-21 21:05:07 +0000

    search and browse

--- resources/views/search/search.blade.php
+++ resources/views/search/search.blade.php
@@ -1,14 +1,16 @@
+<link rel="stylesheet" href="{{ URL::asset('css/search.css') }}">
+
 @extends('layouts.app')
 @section('content')
 <body>
-<div class="container">
-    search tag
-    {{Form::open(array('url' =>'/search'))}}
-      {{Form::text('keyword', null, array('placeholder'=>'search by keyword'))}}
-      {{Form::submit('search')}}
-    {{Form::close()}}
 
-</div>
+    <div class="search">
+        Find stories and pictures by tag (comma separated)
+        {{Form::open(array('url' =>'/search'))}}
+            {{Form::text('keyword', null, array('class'=>'searchbox', 'placeholder'=>'search by tag'))}}
+            {{Form::submit('search')}}
+        {{Form::close()}}
+    </div>
 
 </body>
 </html>
