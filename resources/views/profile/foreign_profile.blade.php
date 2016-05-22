b7fefcc103ae31b7c5356174db8ef983221c8606
David Ding <davidxyding1@gmail.com>
2016-05-22 19:40:04 +0000

    getting stuff from Chris

--- resources/views/profile/foreign_profile.blade.php
+++ resources/views/profile/foreign_profile.blade.php
@@ -1,5 +1,24 @@
 @extends('layouts.app')
 <!DOCTYPE html>
+<style>
+.userbar {
+    position: relative;
+    padding: 5px;
+    width: 100%;
+    max-height: 300px;
+    border-radius: 10px;
+    background: rgba(211, 211, 211, 0.5);
+}
+h1 {
+    font-size: 24;
+    font-weight: bold;
+}
+h3 {
+    font-size: 20;
+    font-style: italic;
+}
+</style>
+
 <html lang="en">
 	<script src ="http://code.jquery.com/jquery-1.11.1.js "> </script>
 	<script>
@@ -95,12 +114,17 @@
 @else
 	<div class="container">
 		@if ($User->username !=  Auth::user()->username)
-        <div class="row">
-            <h1 class="col-md-9">
-                {{$User->username}}
-            </h1>
+        <div class="row userbar">
+            <div class="col-md-10">
+                <h1>
+                    {{$User->username}}
+                </h1>
+                <h3>
+                    {{$User->name}}
+                </h3>
+            </div>
 
-    		<div class="col-md-3">
+    		<div class="col-md-2">
         		@if ($IsFollowed)
         			<form>         
         			<input type="submit" id="unfollow" value ="Unfollow" onclick ="return unfollowuser()">
